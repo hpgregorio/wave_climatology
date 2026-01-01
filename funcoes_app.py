8959b70df6fbadc9802236c1dead94a402018d94
hpgregorio <helvio@hpgregorio.net>
2025-12-31 19:07:56 -0600

    Update funcoes_app.py

--- funcoes_app.py
+++ funcoes_app.py
@@ -912,7 +912,7 @@ def plot_wind_hours(df_locais, df, selected_years, bins, labels, parametro, nome
 		)
 
 	hours_ticks = [f'{hour:02d} h' for hour in hours_loc]
-	layout.update(xaxis=dict(tickvals=hours_loc, ticktext=hours_ticks), tickfont=dict(size=8))
+	layout.update(xaxis=dict(tickvals=hours_loc, ticktext=hours_ticks, tickfont=dict(size=8)))
 
 	fig = go.Figure(data=traces, layout=layout)
 	
@@ -1086,8 +1086,8 @@ def plot_others_hour(df_locais, df, selected_years, selected_location, mes, prec
 		)
 		
 		hours_ticks = [f'{hour:02d} h' for hour in hours_loc]
-		fig.update_layout(xaxis=dict(tickvals=hours_loc, ticktext=hours_ticks), tickfont=dict(size=8))
-		fig.update_layout(xaxis2=dict(tickvals=hours_loc, ticktext=hours_ticks), tickfont=dict(size=8))
+		fig.update_layout(xaxis=dict(tickvals=hours_loc, ticktext=hours_ticks, tickfont=dict(size=8)))
+		fig.update_layout(xaxis2=dict(tickvals=hours_loc, ticktext=hours_ticks, tickfont=dict(size=8)))
 		
 		fig.add_trace(trace_prec_hist, row=1, col=1)
 		fig.add_trace(trace_prec, row=1, col=1)
@@ -1147,8 +1147,8 @@ def plot_others_hour(df_locais, df, selected_years, selected_location, mes, prec
 		)
 		
 		hours_ticks = [f'{hour:02d} h' for hour in hours_loc]
-		fig.update_layout(xaxis=dict(tickvals=hours_loc, ticktext=hours_ticks), tickfont=dict(size=8))
-		fig.update_layout(xaxis2=dict(tickvals=hours_loc, ticktext=hours_ticks), tickfont=dict(size=8))
+		fig.update_layout(xaxis=dict(tickvals=hours_loc, ticktext=hours_ticks, tickfont=dict(size=8)))
+		fig.update_layout(xaxis2=dict(tickvals=hours_loc, ticktext=hours_ticks, tickfont=dict(size=8)))
 	
 		fig.add_trace(trace_prec)
 		fig.add_trace(trace_temp)
